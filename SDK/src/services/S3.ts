df34a2ffe6a8ceea80af3762eef3692d842e448e
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-02-13 20:24:31 +0530

    Added Get all Files  in sdk

---
+++ SDK/src/services/S3.ts
@@ -0,0 +1,54 @@
+class Harsh_S3 {
+  private key: string;
+  private secret: string;
+  constructor(key: string, secret: string) {
+    if (!key || !secret) {
+      throw new Error("Please provide key and secret");
+    }
+    if (key !== "harsh" && secret !== "harsh") {
+      throw new Error("Invalid key or secret");
+    }
+    this.key = key;
+    this.secret = secret;
+  }
+
+  async uploadFile(
+    file: File,
+    bucketId: string,
+    path: string,
+    filename: string
+  ) {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("path", path);
+      formData.append("filename", filename);
+
+      const data = await fetch(
+        `http://localhost:4001/api/add-files/${bucketId}?path=${path}&filename=${filename}`,
+        {
+          method: "PUT",
+          body: formData,
+        }
+      );
+      const parsedData = await data.json();
+      return parsedData;
+    } catch (e: any) {
+      console.error(e);
+    }
+  }
+
+  async getAllFiles(bucketId: string) {
+    try {
+      const data = await fetch(
+        `http://localhost:4001/api/get-files/${bucketId}`
+      );
+      const parsedData = await data.json();
+      return parsedData;
+    } catch (e: any) {
+      console.error(e);
+    }
+  }
+}
+
+export { Harsh_S3 };
--- Test-SDK/src/index.ts
+++ Test-SDK/src/index.ts
@@ -3,11 +3,26 @@ import fs from "fs";
 import path from "path";
 
 const s3 = new Harsh_S3("harsh", "harsh");
-const fileName = "index.ts";
-const filePath = "src/" + fileName;
-const data = fs.readFileSync(filePath);
-const buffer = Buffer.from(data);
-const file = new File([buffer], fileName, {
-  type: "application/octet-stream",
-});
-s3.uploadFile(file, "59722c6f-de9e-4073-b4a7-e30a33991570", "", "test.txt");
+const bucketId = "59722c6f-de9e-4073-b4a7-e30a33991570";
+
+const uploadFile = async () => {
+  const fileName = "index.ts";
+  const filePath = "src/" + fileName;
+  const data = fs.readFileSync(filePath);
+  const buffer = Buffer.from(data);
+  const file = new File([buffer], fileName, {
+    type: "application/octet-stream",
+  });
+  await s3.uploadFile(file, bucketId, "", "test.txt");
+};
+
+const getAllFiles = async () => {
+  const { files, message } = await s3.getAllFiles(bucketId);
+  console.log(files);
+  files.forEach((file: { file: string; fileName: string; data: string }) => {
+    const filePath = path.join(__dirname, file.file);
+    fs.writeFileSync(filePath, file.data);
+  });
+};
+
+getAllFiles();
