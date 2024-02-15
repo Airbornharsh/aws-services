aee0b233c65f850abbdf38af04aa9cfd415fff2b
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-02-15 11:40:33 +0530

    Updated

--- SDK/src/index.ts
+++ SDK/src/index.ts
@@ -1,4 +1,4 @@
 import { Harsh_S3 } from "./services/S3";
 import { Harsh_Lambda } from "./services/Lambda";
 import { Harsh_DynamoDB } from "./services/DynamoDB";
-export { Harsh_S3, Harsh_Lambda, Harsh_DynamoDB };
+export { Harsh_S3, Harsh_Lambda, Harsh_DynamoDB }; 
